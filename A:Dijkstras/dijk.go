d818265c2e4eff67bfa2c949890c6d4c84477c26
Hallmane <ericssonwilliam96@gmail.com>
2022-10-29 14:17:44 +0200

    bojangles

--- A:Dijkstras/dijk.go
+++ A:Dijkstras/dijk.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
-func (m Map) dijkstra(start *City, end *City) {
+func (m Map) dijkstra(start *City, end *City) (way []string, cost int) {
 
 	pq := newQueue()             // queue for cities to explore
 	explored := map[*City]bool{} // cities visited
-	pq.Set(start, 0)             // initial city
+	trace := make(map[*City]int)
+
+	pq.Set(start, 0) // initial city
 
 	for !pq.Empty() {
 
@@ -17,8 +19,15 @@ func (m Map) dijkstra(start *City, end *City) {
 		// create path struct from current city and travel time
 		path := Path{cur_city, time_sum}
 
-		if cur_city == end {
-			// back-trace path
+		if cur_city == end { // then back trace path
+			cost = path.travel_time
+			p_city := path.city
+
+			for p_city != start {
+				way = append(way, p_city.name)
+				p_city = trace[p_city.name]
+				fmt.Println(p_city)
+			}
 			break
 		}
 
@@ -33,20 +42,31 @@ func (m Map) dijkstra(start *City, end *City) {
 
 			// if not in queue, add with travel time
 			if _, ok := pq.Get(conn.city); !ok {
-				pq.Set(conn.city, conn.distance)
+				trace[conn.city.name] = conn.city // for backtracing
+				pq.Set(conn.city, conn.distance)  // add to queue with time
 				fmt.Println(conn.city, " added..")
 				continue
 			}
 
 			fmt.Printf("-> %s ", conn.city.name)
 			fmt.Println("old time:", path.travel_time)
+
 			new_time := path.travel_time + conn.distance
 			fmt.Println("new time:", new_time)
 
 			if new_time < path.travel_time {
+				trace[conn.city.name] = conn.city
 				pq.Set(conn.city, new_time)
 			}
 		}
 	}
+
+	way = append(way, start.name)
+
+	for i, j := 0, len(way)-1; i < j; i, j = i+1, j-1 {
+		way[i], way[j] = way[j], way[i]
+	}
+
+	return way, cost
 
 }
